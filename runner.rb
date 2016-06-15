834baba829c342ef73272c1490504b0905722ce3
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-15 16:31:33 -0400

    added 4 files.

---
+++ runner.rb
@@ -0,0 +1,28 @@
+
+require_relative './game.rb'
+require_relative './players.rb'
+require_relative './servobj.rb'
+
+class Runner
+  MoveMaker.welcome
+  MoveMaker.start
+  MoveMaker.play
+end
+
+
+
+
+
+
+
+#------------JUNK BELOW-------------#
+  # humanMarker = 'X'
+  # compMarker ='O'
+  # puts "Hello #{name}! Please choose X or O."
+  # symbols = ['X','O']
+  # humanMarker = gets.chomp
+  # compMarker = symbols - humanMarker.split
+  # compMarker = compMarker.join
+
+
+  #Game.new(@human, @computer).playAgain#(@human, @computer)
---
+++ servobj.rb
@@ -0,0 +1,40 @@
+class MoveMaker
+    attr_accessor :human
+
+    def initialize(human, game)
+      @human = human
+    end
+
+    def welcome
+      puts "Welcome to tic-tac-toe! Please enter your name."
+      #getName
+      @name =  gets.chomp
+      puts "Hello #{name}! You're playing as X."
+      @human = Human.new(@name, 'X')
+      @human.add_game(@game) #adding game to the player's profile
+      @computer = Computer.new('O')
+      @game = Game.new(@human, @computer)
+    end
+
+    # def getName
+    #
+    # end
+
+    def self.start
+      puts "Below is your game board."
+      @game.printBoard
+      puts "It's your turn."
+    end
+
+    def self.play
+      @game.makeMove
+      until @game.checkWinner == true || @game.draw == true
+        @game.makeMove
+      end
+    end
+
+    # def self.exit
+    #
+    # end
+
+end
